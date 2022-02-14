10857c9ba9af38a2ee910c851690259629172a09
Pablo Sanchez <pablosgrad12@gmail.com>
2022-02-14 13:24:19 -0700

    Setup to read from file.

--- shipsSortingAndSearching/shipSearch.cpp
+++ shipsSortingAndSearching/shipSearch.cpp
@@ -4,10 +4,6 @@
 #include <fstream>
 using namespace std;
 
-//This vector is made up of "ship" data types and is 
-//needed to pass to the sorting functions.
-vector<ship> ships;
-
 //"ship" datatype that will be used to make 
 //various ships from a file.
 struct ship {
@@ -17,10 +13,50 @@ struct ship {
 	int size;
 };
 
-int main() {
-	
-	fstream readFile;
-	readFile.open("ships.txt", ios::out);
+//This vector is made up of "ship" data types 
+//that will be added from a file and is 
+// needed to pass to the sorting functions.
+vector<ship> ships;
 
-	
+int main() {
+	//First, all of the data for each ship will
+	//be stored into a file and read from it to
+	//be stored into a vector.
+	fstream readFile;
+	readFile.open("ships.txt", ios::in);
+
+	if (readFile.is_open()) {
+		//This while loop reads from the file until
+		//the end of the file.
+		while(readFile.good()) {
+			//Local variables to be used to get the info 
+			//of the various ships from the file one ship 
+			//at a time.
+			string shipName;
+			string shipType;
+			string shipServiceNum;
+			string shipSize;
+			//shipSize is a string for now because getline
+			//only accepts a string as a parameter.
+
+			getline(readFile, shipName);
+			getline(readFile, shipType);
+			getline(readFile, shipServiceNum);
+			getline(readFile, shipSize);
+
+			//Storing values from the file onto a
+			//struct.
+			ship ship;
+			ship.name = shipName;
+			ship.type = shipType;
+			ship.serviceNum = shipServiceNum;
+			ship.size = stoi(shipSize);
+			//stoi() turns the string shipSize into an int.
+
+			//struct is added to the vector of ships.
+			ships.push_back(ship);
+			
+		}
+		readFile.close();
+	}
 }
