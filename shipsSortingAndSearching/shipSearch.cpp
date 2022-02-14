73fe19b3117fa7dde15303f0f8ed8dced3dfbdfe
Pablo Sanchez <pablosgrad12@gmail.com>
2022-02-14 09:19:17 -0700

    Makes struct used for ships and opens file.

--- shipsSortingAndSearching/shipSearch.cpp
+++ shipsSortingAndSearching/shipSearch.cpp
@@ -1,1 +1,26 @@
-#include "shipSearch.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//This vector is made up of "ship" data types and is 
+//needed to pass to the sorting functions.
+vector<ship> ships;
+
+//"ship" datatype that will be used to make 
+//various ships from a file.
+struct ship {
+	string name;
+	string type;
+	string serviceNum;
+	int size;
+};
+
+int main() {
+	
+	fstream readFile;
+	readFile.open("ships.txt", ios::out);
+
+	
+}
