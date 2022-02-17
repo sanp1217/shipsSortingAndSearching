83a790fb3a1a3e67aa639ce38888c7b801c548e3
Pablo Sanchez <pablosgrad12@gmail.com>
2022-02-17 13:21:55 -0700

    Makes function to sort by type.

--- shipsSortingAndSearching/shipSearch.cpp
+++ shipsSortingAndSearching/shipSearch.cpp
@@ -20,6 +20,7 @@ const int NUM_OF_SHIPS = 10;
 ship ships[NUM_OF_SHIPS];
 
 //function prototypes
+void sortByType(ship[], int);
 void showShips(ship[], int);
 
 
@@ -55,16 +56,34 @@ int main() {
 		}
 		readFile.close();
 	}
+	
 }
 
-
-void showShips(ship array[], int num) {
+//Outputs ship info, spaced evenly
+void showShips(ship array[], int numOfShips) {
 	cout << left << setw(20) << "Ship Name" << setw(20) << "Type" << setw(20) << "Service Number" << setw(20) << "Size" << endl;
 	cout << string(70, '*') << endl;
-	for (int x = 0; x < num; x++) {
+	for (int x = 0; x < numOfShips; x++) {
 		cout << left << setfill(' ')
 		<< setw(20) << array[x].name << setw(20) << array[x].type << setw(20) << array[x].serviceNum << setw(20) << array[x].size << endl;
 	}
 	cout << endl;
 }
 
+//sorts ships by type, alphabetically using selection sort.
+void sortByType(ship array[], int numOfShips) {
+	int startScan, minIndex;
+	ship minValue;
+	for (startScan = 0; startScan < (numOfShips - 1); startScan++) {
+		minIndex = startScan;
+		minValue = array[startScan];
+		for (int index = startScan + 1; index < numOfShips; index++) {
+			if (array[index].type < minValue.type) {
+				minValue = array[index];
+				minIndex = index;
+			}
+		}
+		array[minIndex] = array[startScan];
+		array[startScan] = minValue;
+	}
+}
